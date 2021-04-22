05fcdfc076020947583beae1a2dca847c0e9c70c
Jashan Khela <jashan.khela@hotmail.com>
2021-04-22 12:59:32 -0700

    Update firebase.js
    Remove Config Files From Private Repo Clone

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,6 @@ import 'firebase/auth';
 // import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: 'AIzaSyCyDFH3Yi_bKpTwRTc7AXqMmmBWhuGtWKk',
-  authDomain: 'instagram-b3dd4.firebaseapp.com',
-  projectId: 'instagram-b3dd4',
-  storageBucket: 'instagram-b3dd4.appspot.com',
-  messagingSenderId: '467154664582',
-  appId: '1:467154664582:web:96bcdc93e0be86e565e507',
 };
 
 const firebase = Firebase.initializeApp(config);
